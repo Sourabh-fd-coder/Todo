91a58d7c51af0babc9b747ea81c803fa51e07b6b
sourabh2989 <shyadav2989@gmail.com>
2024-08-13 14:14:38 +0530

    fsdddsd

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  // const deleteTodo = (index) => {
+  //   const updatedTodos = todos.filter((_, i) => i !== index);
+  //   setTodos(updatedTodos);
+  // };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} 
+            {/* <button onClick={() => deleteTodo(index)}>Delete</button> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
